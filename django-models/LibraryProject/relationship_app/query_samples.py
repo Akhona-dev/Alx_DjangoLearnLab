de231016511ca298a8b241b81cc07eb91e6fb0bd
Akhona Trom <akhonatrom1@gmail.com>
2025-07-27 21:23:28 +0200

    add new things

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,11 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
+author_name = "J.K. Rowling"   
+library_name = "Central Library"   
+
+
 try:
-    author = Author.objects.get(name="J.K. Rowling")
+    author = Author.objects.get(name=author_name)
     books_by_author = Book.objects.filter(author=author)
     print(f"Books by {author.name}:")
     for book in books_by_author:
@@ -10,7 +14,7 @@ except Author.DoesNotExist:
     print("Author not found.")
 
 try:
-    library = Library.objects.get(name="Central Library")
+    library = Library.objects.get(name=library_name)  
     books_in_library = library.books.all()
     print(f"\nBooks in {library.name}:")
     for book in books_in_library:
@@ -18,8 +22,9 @@ try:
 except Library.DoesNotExist:
     print("Library not found.")
 
+
 try:
-    library = Library.objects.get(name="Central Library")  
+    library = Library.objects.get(name=library_name) 
     librarian = Librarian.objects.get(library=library)
     print(f"\nLibrarian for {library.name}: {librarian.name}")
 except (Library.DoesNotExist, Librarian.DoesNotExist):
