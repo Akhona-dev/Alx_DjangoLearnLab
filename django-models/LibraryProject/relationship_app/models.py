fb46b68afaf01c656415c4d67c99dfe82e6f44fc
Akhona Trom <akhonatrom1@gmail.com>
2025-08-03 15:31:10 +0200

    add changes

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,20 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Table 1
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+#--------------------------------------------
 # Table 2
+#--------------------------------------------
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+#--------------------------------------------------
 # Table 3
+#-----------------------------------------------------
+
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
+#--------------------------------------------------
 # Table 4
+#--------------------------------------------------
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+#------------------------------------------------------
+#Table 5
+#-------------------------------------------------------
+
+class UserProfile(models.Model):                
+
+    ROLE_CHOICES = [                            
+        ('Admin', 'Admin'),                     
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+
+    user = models.OneToOneField(               
+        User,                                   
+        on_delete=models.CASCADE                
+    )
+
+    role = models.CharField(                   
+        max_length=20,                          
+        choices=ROLE_CHOICES ,
+        default='Member'                   
+    )
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
-from . import views
+from . import views, admin_view, librarian_view, member_view
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books-available/', views.show_books, name='show-books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-details'),
-
-    # Registration (custom view, must be called "views.register")
     path('register/', views.register, name='register'),
-
-    # Login using Django's built-in LoginView
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-
-    # Logout using Django's built-in LogoutView
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    path('admin/', admin_view.admin_view, name='admin-view'),
+    path('librarian/', librarian_view.librarian_view, name='librarian-view'),
+    path('member/', member_view.member_view, name='member-view'),
 ]
