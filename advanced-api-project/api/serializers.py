76fb73830f1e5df937feec62e0e3c07bfa163d53
Akhona Trom <akhonatrom1@gmail.com>
2025-08-17 06:17:49 +0200

    add changes

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Author
 from .models import Book
-from rest_framework.serializers import ValidationError
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,7 +9,7 @@ class BookSerializer(serializers.ModelSerializer):
 
     def validate_publication_year(self, value):
         if value > 2025:  
-            raise ValidationError("Publication year cannot be in the future!")
+            raise serializers.ValidationError("Publication year cannot be in the future!")
         return value
 
 class AuthorSerializer(serializers.ModelSerializer):
