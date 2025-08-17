e1465d99334677856ae21e53ca8e64a9eeb2d8df
Akhona Trom <akhonatrom1@gmail.com>
2025-08-17 06:11:09 +0200

    add changes

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,15 +1,21 @@
 from rest_framework import serializers
 from .models import Author
 from .models import Book
+from rest_framework.serializers import ValidationError
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Book  # specifies which model to use
-        fields = '__all__'  # allows cornversion of all columns/fields
+        model = Book
+        fields = '__all__' # allows conversion of columns/fields
+
+    def validate_publication_year(self, value):
+        if value > 2025:  
+            raise ValidationError("Publication year cannot be in the future!")
+        return value
 
 class AuthorSerializer(serializers.ModelSerializer):
-    books = BookSerializer(many=True, read_only=True)  # nested serializer for related books
+    books = BookSerializer(many=True, read_only=True)
 
     class Meta:
         model = Author
-        fields = ['name', 'books']  # includes specific colunns
+        fields = ['name', 'books'] #specifies which field to convert
