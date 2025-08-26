cd8cd8223066b2998af15d6dd2611f9ce2222917
Akhona Trom <akhonatrom1@gmail.com>
2025-08-26 12:12:44 +0200

    add changes

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 from .models import Post, Like
 from .serializers import PostSerializer
+from notifications.models import Notification   # 👈 import Notification
+
 
 class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -23,10 +24,17 @@ class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)  # 👈 use generics.get_object_or_404
         like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if created:
+            # 👇 create a notification for the post author
+            Notification.objects.create(
+                recipient=post.author,
+                actor=request.user,
+                verb="liked your post",
+                target=post
+            )
             return Response({"message": "Post liked!"}, status=status.HTTP_201_CREATED)
         else:
             like.delete()
